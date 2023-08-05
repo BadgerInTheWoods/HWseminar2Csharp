03fc15cd7f7a19a386db6723f7a837b67ac41a02
AlexanderBadger <sashazateev444@gmail.com>
2023-08-05 18:59:39 +0200

    done

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99)
+{
+    int third = number % 10;
+    Console.WriteLine($"третье число = {third}");
+}
+else
+{
+    Console.WriteLine("третьего числа нет");
+}
---
+++ task3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+Console.WriteLine("введите день недели");
+int dayWeek = Convert.ToInt32(Console.ReadLine());
+if (dayWeek < 8 && dayWeek > 0)
+{
+    if (dayWeek == 6 || dayWeek == 7)
+    {
+        Console.WriteLine("да, выходной");
+    }
+    else
+    {
+        Console.WriteLine("нет,не выходной");
+    }
+}
+else
+{
+    Console.WriteLine("неверное число");
+}
